5c3ba174ff7f05cea71bf88463a500d33dbae6ad
Sathya Narayan L <59957985+sathya-tech@users.noreply.github.com>
2024-06-17 23:12:21 +0530

    Update Footer.jsx

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => {
         </div>
         <div className="app__footer-card">
           <img src={images.mobile} alt="phone" />
-          <a href="tel:+1 (123) 456-7890" className="p-text">+1 (123) 456-7890</a>
+          <a href="tel:+91 8618677467" className="p-text">+91 8618677467</a>
         </div>
       </div>
       {!isFormSubmitted ? (
